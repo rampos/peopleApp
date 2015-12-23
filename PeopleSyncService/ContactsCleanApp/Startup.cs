99568e4278ace8f30cf21e8fc68e9027eedb81ce
Ram Poornalingam <rampos@gmail.com>
2015-12-23 15:15:06 -0800

    added oauth support

---
+++ PeopleSyncService/ContactsCleanApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactsCleanApp.Startup))]
+namespace ContactsCleanApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ PeopleSyncService/src/PeopleSyncService/OutlookHelpers/OutlookRestClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+public class OutlookRestClient
+{
+    string endpoint = "https://outlook.office.com/api/v1.0";
+    string ApplicationID = "a1fea394 - 9be2-4a3d-b70e-b1d88c405dc0";
+    string AppSecret = "QAjAKVAK8gu1cXkdEOiVy1f";
+
+}
